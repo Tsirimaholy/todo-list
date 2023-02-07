d344a87308078e3bec0a652da915d2dc29605498
Tsirimaholy <hei.tsirimaholy@gmail.com>
2023-02-07 13:18:21 +0300

    refactor: reorganizing code

--- src/components/todo-form.tsx
+++ src/components/todo-form.tsx
@@ -2,13 +2,14 @@ import React, {FC, useState} from "react";
 import {TTask} from "./todo-list";
 
 type TTodoForm = {
-    onSubmit: (task: TTask) => void
+    onSubmit: (task: TTask) => void;
+    defaultValue?: string;
 }
-export const TodoForm: FC<TTodoForm> = (props) => {
-    const [currentTask, setCurrentTask] = useState<TTask>({name: "", done: false});
+export const TodoForm: FC<TTodoForm> = ({onSubmit, defaultValue}): React.ReactElement => {
+    const [currentTask, setCurrentTask] = useState<TTask>({name: defaultValue || "", done: false});
     const handleAdd = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key !== 'Enter' && currentTask.name.length > 0) return
-        props.onSubmit(currentTask);
+        onSubmit(currentTask);
         // reset the current task value
         setCurrentTask({name: "", done: false})
     }
@@ -22,6 +23,7 @@ export const TodoForm: FC<TTodoForm> = (props) => {
         <div className="todo_list__form">
             <input
                 type="text"
+                placeholder="todo-input"
                 onKeyDown={handleAdd}
                 onChange={handleChange}
                 value={currentTask.name}
