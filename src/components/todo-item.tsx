277ea1c5551ecba9e92b34eda45b531e5b54efdf
Tsirimaholy <hei.tsirimaholy@gmail.com>
2023-02-07 20:08:14 +0300

    fix: do not display checkbox on a done item

--- src/components/todo-item.tsx
+++ src/components/todo-item.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import {TTask} from "./todo-list";
 
-type TodoItemProps = { isDone?: boolean, onClick: () => void, task: TTask };
+type TodoItemProps = { onClick: () => void, task: TTask };
 export const TodoItem = (props: TodoItemProps) => {
-    const {isDone = false, onClick, task} = props;
+    const {onClick, task} = props;
 
 
     return (<>
-        {!isDone && <input type={"checkbox"} onClick={onClick}/>}
+        {!task.done && <input type={"checkbox"} onClick={onClick}/>}
         <span>{task.name}</span>
     </>)
 };
